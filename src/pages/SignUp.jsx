9dcbfec55012a84e4f509d0a0370cd325b8b23a6
Harry MacCode <emarh.harry.code@gmail.com>
2023-10-13 16:08:01 +0200

    Install firebase, react-toastify and signup the user

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
 import { AiFillEyeInvisible, AiFillEye } from 'react-icons/ai';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import OAuth from '../components/OAuth';
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { db } from '../firebase';
+import { doc, serverTimestamp, setDoc } from 'firebase/firestore';
+import { toast } from 'react-toastify';
 
 const SignUp = () => {
+  const navigate = useNavigate();
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
     name: '',
@@ -17,6 +22,28 @@ const SignUp = () => {
       [e.target.id]: e.target.value
     }));
   };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+      const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
+      updateProfile(auth.currentUser, { displayName: name });
+      const user = userCredentials.user;
+      const formDataCopy = {...formData};
+      delete formDataCopy.password;
+      formDataCopy.timestamp = serverTimestamp();
+
+      await setDoc(doc(db, 'users', user.uid), formDataCopy);
+      toast.success('Sign up was successful');
+      navigate('/');
+      // console.log(user);
+    } catch(error) {
+      // console.log(error);
+      toast.error('Something went wrong with the registration');
+    }
+  }
   return (
     <section>
       <h1 className='text-3xl text-center mt-6 font-bold'>
@@ -28,7 +55,7 @@ const SignUp = () => {
             alt="connection" className='w-full rounded-2xl' />
         </div>
         <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-          <form>
+          <form onSubmit={handleSubmit}>
           <div>
               <input className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out' type="text" id='name' 
                 value={name} onChange={handleChange} placeholder='Full name' />
